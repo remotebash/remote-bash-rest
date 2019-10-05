1368084b0d71b63123cb79941d291de3014237f7
mistless <lari3573@outlook.com>
2019-10-05 17:59:29 -0300

    Refatoração

--- api/src/main/java/com/remotebash/api/exception/RegisterException.java
+++ api/src/main/java/com/remotebash/api/exception/RegisterException.java
@@ -4,10 +4,10 @@ public abstract class RegisterException extends Throwable {
 	
 	private static final long serialVersionUID = -6027937808735719202L;
 	
-	private final String mensagem;
+	private final String message;
 
 	public RegisterException(String mensagem) {
-		this.mensagem = mensagem;
+		this.message = mensagem;
 	} 
 	
 }

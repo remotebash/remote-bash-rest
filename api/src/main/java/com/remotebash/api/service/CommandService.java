973ca1939fec937cc7a03d91a81ab02b301b37a3
mateuscovos <mateus.covos@gmail.com>
2019-11-30 14:54:48 -0300

    ajuste conflito no MediaType

--- api/src/main/java/com/remotebash/api/service/CommandService.java
+++ api/src/main/java/com/remotebash/api/service/CommandService.java
@@ -3,16 +3,9 @@ package com.remotebash.api.service;
 import java.util.Date;
 import java.util.UUID;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
